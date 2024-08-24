f4927891a06541c0b019a60aa77a0fdf8b9d4a41
poeticcode01 <cmahto01@gmail.com>
2024-08-24 16:26:34 +0530

    respond to ping

--- app/server.go
+++ app/server.go
@@ -6,20 +6,58 @@ import (
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func  handleClient(conn net.Conn){
+	// ensure we close the connection after we're done
+	defer conn.Close()
 
+	// Read data
+	buf := make([]byte,1024)
+	n,err := conn.Read(buf)
+
+	if err != nil{
+		fmt.Println("Error reading data from  the client",err.Error())
+		return
+	}
+	fmt.Println("Received data",string(buf[:n]))
+
+
+	message := []byte("Hello , server!")
+	n,err := conn.Write(message)
+
+	if err != nil{
+		fmt.Println("Error sneding message to the  client",err.Error())
+		return
+	}
+	fmt.Println("send %d bytes",n)
+
+
+
+}
+
+func main() {
 	
-	
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	// ensure to stop the tcp server when the program exits
+	defer listener.close()
+
+	fmt.Println("Server is listening on port 6379")
+	for {
+		// Block until we receive an incoming connection
+		conn, err = listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+
+		}
+		// Hnadle client connection
+		handleClient(conn)
+
+		
 	}
+	
+
 }
